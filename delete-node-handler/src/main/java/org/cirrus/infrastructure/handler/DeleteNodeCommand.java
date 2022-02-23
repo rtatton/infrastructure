21a6433d84edede39edb0998a1262934683a5dbd
rtatton <rdt17@protonmail.com>
2022-02-23 16:44:07 -0500

    Update Javadoc
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeCommand.java
+++ delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeCommand.java
@@ -5,12 +5,10 @@ import java.util.concurrent.CompletionException;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.cirrus.infrastructure.handler.exception.FailedEventSourceMappingException;
-import org.cirrus.infrastructure.handler.exception.FailedResourceCreationException;
 import org.cirrus.infrastructure.handler.exception.FailedResourceDeletionException;
+import org.cirrus.infrastructure.handler.exception.FailedStorageDeleteException;
 import org.cirrus.infrastructure.handler.exception.FailedStorageReadException;
-import org.cirrus.infrastructure.handler.exception.FailedStorageWriteException;
-import org.cirrus.infrastructure.handler.exception.NodeAlreadyExistsException;
+import org.cirrus.infrastructure.handler.exception.NoSuchNodeException;
 import org.cirrus.infrastructure.handler.model.DeleteNodeRequest;
 import org.cirrus.infrastructure.handler.model.NodeRecord;
 import org.cirrus.infrastructure.handler.service.FunctionService;
@@ -44,28 +42,17 @@ public class DeleteNodeCommand implements Command<DeleteNodeRequest, Void> {
   }
 
   /**
-   * Creates a cloud-based node with computing and messaging capabilities.
+   * Deletes a cloud-based node with computing and messaging capabilities.
    *
-   * @param request Contains the identifier of the node and resource configuration.
-   * @throws NodeAlreadyExistsException Thrown when the requested node identifier already exists.
+   * @param request Contains the identifier of the node to delete.
+   * @throws NoSuchNodeException Thrown when the requested node identifier does not exist.
    * @throws FailedStorageReadException Thrown when an error occurs when attempting to access the
-   *     storage service to check if the requested node identifier already exists.
-   * @throws FailedResourceCreationException Thrown when any of the resources fail to be created.
-   * @throws FailedResourceDeletionException Thrown when any of the created cloud resources fail to
-   *     be deleted. During creation of the node, this is thrown when attempting to rollback after
-   *     either (1) only some node resources could be successfully created or (2) any of the
-   *     proceeding steps, after creating the node resources, fails.
-   * @throws FailedEventSourceMappingException Thrown after successfully creating resources, but
-   *     failing to add the node queue to the node function as an event source mapping. If, in the
-   *     processing of rolling back, one of the created resources fails to be deleted, this
-   *     exception is overridden by a {@link FailedResourceDeletionException}.
-   * @throws FailedStorageWriteException Thrown after successfully creating resources and adding the
-   *     queue as an event source mapping to the function, but failing to store the resource
-   *     identifiers. If, in the processing of rolling back, one of the created resources fails to
-   *     be deleted, this exception is overridden by a {@link FailedResourceDeletionException}.
-   * @return A response containing the resource identifiers of the node.
+   *     storage service to retrieve requested node resource identifiers. * @throws
+   * @throws FailedStorageDeleteException Thrown when an error occurs when attempting to access the
+   *     storage service to delete the requested node resource identifiers.
+   * @throws FailedResourceDeletionException Thrown when any of cloud resources fail to be deleted.
+   * @return Null
    */
-  // TODO Update Javadoc
   public Void run(DeleteNodeRequest request) {
     try {
       CompletableFuture<NodeRecord> getRecord = deleteRecord(request.nodeId());
