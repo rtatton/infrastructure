1e22eac80ef53dfc47bb42dcba575e6529095f5a
rtatton <rdt17@protonmail.com>
2022-02-27 20:55:51 -0500

    Refactor handlers to use API response
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- create-node-handler/src/main/java/org/cirrus/infrastructure/handler/CreateNodeHandler.java
+++ create-node-handler/src/main/java/org/cirrus/infrastructure/handler/CreateNodeHandler.java
@@ -3,13 +3,19 @@ package org.cirrus.infrastructure.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
-public class CreateNodeHandler implements RequestHandler<APIGatewayV2HTTPEvent, String> {
+public class CreateNodeHandler
+    implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
   private static final Command<?, ?> COMMAND = CreateNodeCommand.getInstance();
 
   @Override
-  public String handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    return COMMAND.runFromString(event.getBody());
+  public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+    return APIGatewayV2HTTPResponse.builder()
+        .withBody(COMMAND.runFromString(event.getBody()))
+        .withStatusCode(200)
+        .withIsBase64Encoded(false)
+        .build();
   }
 }
--- delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeHandler.java
+++ delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeHandler.java
@@ -3,15 +3,19 @@ package org.cirrus.infrastructure.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
-public class DeleteNodeHandler implements RequestHandler<APIGatewayV2HTTPEvent, Void> {
+public class DeleteNodeHandler
+    implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
   private static final Command<?, ?> COMMAND = DeleteNodeCommand.getInstance();
 
   @Override
-  public Void handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    COMMAND.runFromString(event.getBody());
-    // TODO How does API Gateway map null response? Be consistent and just return empty string?
-    return null;
+  public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+    return APIGatewayV2HTTPResponse.builder()
+        .withBody(COMMAND.runFromString(event.getBody()))
+        .withStatusCode(200)
+        .withIsBase64Encoded(false)
+        .build();
   }
 }
