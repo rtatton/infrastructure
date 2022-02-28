f8b20a95eb78cb441ef8756694d8d27831e8bff6
rtatton <rdt17@protonmail.com>
2022-02-28 16:33:13 -0500

    Remove Converter
    Keeping it simple(r)
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- handler-core/src/main/java/org/cirrus/infrastructure/handler/AbstractHandler.java
+++ handler-core/src/main/java/org/cirrus/infrastructure/handler/AbstractHandler.java
@@ -4,26 +4,30 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import org.cirrus.infrastructure.handler.api.ApiGatewayV2EventConverter;
-import org.cirrus.infrastructure.handler.api.ApiGatewayV2ResponseConverter;
 import org.cirrus.infrastructure.handler.api.ApiRequest;
 import org.cirrus.infrastructure.handler.api.ApiResponse;
-import org.cirrus.infrastructure.util.Converter;
 
 public abstract class AbstractHandler
     implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-  private static final Converter<APIGatewayV2HTTPEvent, ApiRequest> EVENT_CONVERTER =
-      new ApiGatewayV2EventConverter();
-  private static final Converter<ApiResponse, APIGatewayV2HTTPResponse> RESPONSE_CONVERTER =
-      new ApiGatewayV2ResponseConverter();
-
   @Override
   public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    ApiRequest request = EVENT_CONVERTER.forward(event);
+    ApiRequest request = mapToRequest(event);
     ApiResponse response = handle(request);
-    return RESPONSE_CONVERTER.forward(response);
+    return mapToResponse(response);
   }
 
   protected abstract ApiResponse handle(ApiRequest request);
+
+  private ApiRequest mapToRequest(APIGatewayV2HTTPEvent event) {
+    return ApiRequest.of(event.getBody());
+  }
+
+  private APIGatewayV2HTTPResponse mapToResponse(ApiResponse response) {
+    return APIGatewayV2HTTPResponse.builder()
+        .withBody(response.body())
+        .withStatusCode(response.status())
+        .withIsBase64Encoded(false)
+        .build();
+  }
 }
--- util/src/main/java/org/cirrus/infrastructure/util/Converter.java
+++
@@ -1,8 +0,0 @@
-package org.cirrus.infrastructure.util;
-
-public interface Converter<T, U> {
-
-  U forward(T value);
-
-  T backward(U value);
-}
