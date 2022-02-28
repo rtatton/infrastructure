41b91f7195abc743454a3d12ffe2ce916c03f4dd
rtatton <rdt17@protonmail.com>
2022-02-28 16:24:59 -0500

    Add AbstractHandler
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- create-node-handler/src/main/java/org/cirrus/infrastructure/handler/CreateNodeHandler.java
+++ create-node-handler/src/main/java/org/cirrus/infrastructure/handler/CreateNodeHandler.java
@@ -1,26 +1,15 @@
 package org.cirrus.infrastructure.handler;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import org.cirrus.infrastructure.handler.api.ApiCommand;
 import org.cirrus.infrastructure.handler.api.ApiRequest;
 import org.cirrus.infrastructure.handler.api.ApiResponse;
 
-public class CreateNodeHandler
-    implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class CreateNodeHandler extends AbstractHandler {
 
   private static final ApiCommand COMMAND = CreateNodeApi.create();
 
   @Override
-  public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    ApiRequest request = ApiRequest.of(event.getBody());
-    ApiResponse response = COMMAND.run(request);
-    return APIGatewayV2HTTPResponse.builder()
-        .withBody(request.body())
-        .withStatusCode(response.status())
-        .withIsBase64Encoded(false)
-        .build();
+  protected ApiResponse handle(ApiRequest request) {
+    return COMMAND.run(request);
   }
 }
--- delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeHandler.java
+++ delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeHandler.java
@@ -1,26 +1,15 @@
 package org.cirrus.infrastructure.handler;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import org.cirrus.infrastructure.handler.api.ApiCommand;
 import org.cirrus.infrastructure.handler.api.ApiRequest;
 import org.cirrus.infrastructure.handler.api.ApiResponse;
 
-public class DeleteNodeHandler
-    implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+public class DeleteNodeHandler extends AbstractHandler {
 
   private static final ApiCommand COMMAND = DeleteNodeApi.create();
 
   @Override
-  public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-    ApiRequest request = ApiRequest.of(event.getBody());
-    ApiResponse response = COMMAND.run(request);
-    return APIGatewayV2HTTPResponse.builder()
-        .withBody(response.body())
-        .withStatusCode(response.status())
-        .withIsBase64Encoded(false)
-        .build();
+  protected ApiResponse handle(ApiRequest request) {
+    return COMMAND.run(request);
   }
 }
