ee4b91881971140a66ed8a8846a8862f634d35d9
rtatton <rdt17@protonmail.com>
2022-02-28 00:04:22 -0500

    Rename NodeRegistryFactory to NodeTableFactory
    Inline constant fields
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- core/src/main/java/org/cirrus/infrastructure/factory/NodeTableFactory.java
+++ core/src/main/java/org/cirrus/infrastructure/factory/NodeTableFactory.java
@@ -8,20 +8,15 @@ import software.amazon.awscdk.services.dynamodb.ITable;
 import software.amazon.awscdk.services.dynamodb.Table;
 import software.constructs.Construct;
 
-public final class NodeRegistryFactory {
-
-  private static final Attribute PARTITION_KEY = getPartitionKey();
-  private static final Number WRITE_CAPACITY = 5;
-  private static final Number READ_CAPACITY = 5;
-  private static final BillingMode BILLING_MODE = BillingMode.PAY_PER_REQUEST;
+public final class NodeTableFactory {
 
   public static ITable create(Construct scope) {
     return Table.Builder.create(scope, Keys.NODE_TABLE_NAME)
         .tableName(Keys.NODE_TABLE_NAME)
-        .partitionKey(PARTITION_KEY)
-        .writeCapacity(WRITE_CAPACITY)
-        .readCapacity(READ_CAPACITY)
-        .billingMode(BILLING_MODE)
+        .partitionKey(getPartitionKey())
+        .writeCapacity(5)
+        .readCapacity(5)
+        .billingMode(BillingMode.PAY_PER_REQUEST)
         .build();
   }
 
