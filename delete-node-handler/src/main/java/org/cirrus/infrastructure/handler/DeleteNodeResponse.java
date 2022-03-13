9beb5d1a9ee1f9e609738801571011fd6c66d25b
rtatton <rdt17@protonmail.com>
2022-03-12 21:17:56 -0500

    Fixing failing model tests
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeResponse.java
+++ delete-node-handler/src/main/java/org/cirrus/infrastructure/handler/DeleteNodeResponse.java
@@ -1,8 +1,12 @@
 package org.cirrus.infrastructure.handler;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonSerialize(as = ImmutableDeleteNodeResponse.class)
+@JsonDeserialize(as = ImmutableDeleteNodeResponse.class)
 public abstract class DeleteNodeResponse {
 
   public static DeleteNodeResponse create() {
